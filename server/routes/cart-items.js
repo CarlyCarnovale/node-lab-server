a4a019c2b7a4b22cca2ccfaf96c8728f56db5cd6
Carly Carnovale <carlycarnovale@gmail.com>
2019-08-26 09:13:26 -0400

    server

---
+++ server/routes/cart-items.js
@@ -0,0 +1,6 @@
+const cart = [
+  { product: "Alaskan Malamute", id: "hgj876gfh", price: 500, quantity: 1 },
+  { product: "Siberian Husky", id: "ghfghg98gfhg", price: 350, quantity: 2 },
+  { product: "Akita", id: "jhgjgh78bjh", price: 400, quantity: 3 }
+];
+module.exports = cart;
---
+++ server/routes/cart.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cartRoutes = express.Router();
+const cart = require("./cart-items");
+
+
+cartRoutes.get("/cart-items", (req, res) => {
+    res.send(cart);
+});
+
+cartRoutes.post("/cart-items", (req, res) => {
+    console.log(req.body);
+    res.end();
+});
+
+cartRoutes.put("/cart-items/:id", (req, res) => {
+    console.log(req.body, req.params.id);
+    res.end();
+});
+
+cartRoutes.delete("/cart-items/:id", (req, res) => {
+    console.log(req.params.id);
+    res.end();
+});
+
+module.exports = cartRoutes;
